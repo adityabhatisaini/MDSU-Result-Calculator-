e90b1be39cc027fa82517fdac4a91578eb3a86a9
Aditya Bhati <163522402+adityabhatisaini@users.noreply.github.com>
2025-01-21 19:57:21 +0530

    updated

--- Code.py
+++ Code.py
@@ -2,8 +2,7 @@
 def bsc_3(obt_mks):
     total_mks=2025
     cal_per= round(obt_mks/(total_mks/100),2)
-    print(f'Final percentage is:{cal_per}')
-    return total_mks
+    return f'Final percentage is:{cal_per}'
 
 user_input=int(input('Enter your obtained marks'))
-bsc_3(user_input)
+print(bsc_3(user_input))
