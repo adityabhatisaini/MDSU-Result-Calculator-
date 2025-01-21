fa99e6c697e65851798780b87e463c0a30fbd782
Aditya Bhati <163522402+adityabhatisaini@users.noreply.github.com>
2025-01-21 13:41:21 +0000

    updated

--- Code.py
+++ Code.py
@@ -1,8 +1,9 @@
-
+# define all codes in the particular function
 def bsc_3(obt_mks):
     total_mks=2025
-    cal_per= obt_mks/(total_mks/100)
+    cal_per= round(obt_mks/(total_mks/100),2)
     print(f'Final percentage is:{cal_per}')
     return total_mks
 
-bsc_3(1100)
+user_input=int(input('Enter your obtained marks'))
+bsc_3(user_input)
